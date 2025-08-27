304ef3f510d24f1969cc213ae8a07502b6dd2a84
Agustin Monzon Brito <mmonznbrito@frba.utn.edu.ar>
2025-08-27 15:22:22 -0300

    pruebas de optimizacion de video loop

--- nextlevelagency/src/Pages/Home/Home.jsx
+++ nextlevelagency/src/Pages/Home/Home.jsx
@@ -1,13 +1,17 @@
 import styles from "./Home.module.css";
 import Header from "@/Components/Header/Header.jsx";
 import Footer from "@/Components/Footer/Footer.jsx";
+import videoFile from "@/Assets/Videos/201947-916877801_tiny.mp4";
 
 const Home = () => {
   return (
     <>
       <Header />
-      <main>
-        <h1 className={styles.titulo}>Home</h1>
+      <main className={styles.home}>
+        <video muted autoPlay loop className={styles.videoContainer}>
+          <source src={videoFile} type="video/mp4" className={styles.video}/>
+        </video>
+        <div className={styles.capa}></div>
       </main>
       <Footer />
     </>
